7e139c9ee603f4343f09e5e57f295e430f8fa137
wiktor <wiktor.nizio@gmail.com>
2018-06-05 21:00:10 +0200

    Added bitcoin address to every file.

--- events/src/main/kotlin/pl/org/seva/events/event/DatePickerFragment.kt
+++ events/src/main/kotlin/pl/org/seva/events/event/DatePickerFragment.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018 Wiktor Nizio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If you like this program, consider donating bitcoin: 37vHXbpPcDBwcCTpZfjGL63JRwn6FPiXTS
+ */
+
 package pl.org.seva.events.event
 
 import android.widget.DatePicker
@@ -6,7 +25,6 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import java.util.*
-
 
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
