020f06808748f8fe6727e23937204c3c064399a2
wiktor <wiktor.nizio@gmail.com>
2019-03-22 13:46:27 +0100

    Renamed a parameter.

--- events/src/main/kotlin/pl/org/seva/events/main/extension/LiveData.kt
+++ events/src/main/kotlin/pl/org/seva/events/main/extension/LiveData.kt
@@ -23,5 +23,5 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, f: (T) -> Unit) =
-        observe(owner, Observer<T> { f(it) })
+fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) =
+        observe(owner, Observer<T> { observer(it) })
