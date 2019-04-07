9f379e2ee85579617168a25d5235967995066768
wiktor <wiktor.nizio@gmail.com>
2019-04-07 21:10:45 +0200

    Fixed: #25

--- events/src/main/kotlin/pl/org/seva/events/about/AboutFragment.kt
+++ events/src/main/kotlin/pl/org/seva/events/about/AboutFragment.kt
@@ -24,11 +24,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fr_about.*
 import pl.org.seva.events.R
+import pl.org.seva.events.main.extension.inBrowser
 import pl.org.seva.events.main.extension.inflate
 
 class AboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflate(R.layout.fr_about, container)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        github.setOnClickListener { inBrowser(getString(R.string.about_github_url)) }
+    }
 }
