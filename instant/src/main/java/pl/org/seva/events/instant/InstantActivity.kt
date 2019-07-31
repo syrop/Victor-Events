1e127e39468a80f218859fdce61aaa496e449a31
wiktor <wiktor.nizio@gmail.com>
2019-07-31 11:43:24 +0200

    Added InstantActivity.

---
+++ instant/src/main/java/pl/org/seva/events/instant/InstantActivity.kt
@@ -0,0 +1,13 @@
+package pl.org.seva.events.instant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InstantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.ac_instant)
+    }
+}
